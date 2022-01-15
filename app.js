ec3b7968887ec6db663c582681548b1b4b4abccf
LocTran <dinhlockt02@gmail.com>
2022-01-15 17:37:39 +0700

    Set up config

--- app.js
+++ app.js
@@ -1,5 +1,21 @@
 const express = require('express');
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+const api = require('./src/api');
+
+dotenv.config();
 
 const app = express();
 
-app.listen();
+mongoose
+  .connect(process.env.MONGOURI)
+  .then(() => {
+    app.listen(process.env.PORT);
+  })
+  .catch(() => {
+    console.log('Database connection failed');
+  });
+
+app.use(express.json());
+
+app.use('/api', api);
